69dc8b586c32fc089b58ef988418ad3cd9cf870f
rong <rongyy94job@gmail.com>
2021-12-13 10:39:09 +0800

    增加一些東西

--- base.php
+++ base.php
@@ -63,19 +63,205 @@ public function all(...$arg){
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     
     
-}
+
 }
 
-class Journal extends DB{
-protected $table='journal';
-public function __construct(){
-    parent::__construct($this->table);
+// class Journal extends DB{
+// protected $table='journal';
+// public function __construct(){
+//     parent::__construct($this->table);
+
+// public function find(id){ 我希望我只丟id的話會得到這筆資料
+
+
+//我們要只取一筆
+public function find($id){
+    $sql="SELECT * FROM $this->table WHERE ";
+    if(is_array($id)){
+        
+            foreach($id as $key => $value){
+
+                $tmp[]="`$key`='$value'";
+
+            }
+            
+            $sql .= implode(' AND ',$tmp);
+
+    }else{
+
+        $sql .= " id='$id'";
+
+    }
+
+    //echo $sql;
+
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
+      //計算某個欄位或是計算符合條件的筆數
+    //max,min,sum,count,avg
+    public function math($math,$col,...$arg){
+        $sql="SELECT $math($col) FROM $this->table ";
+
+        //依參數數量來決定進行的動作因此使用switch...case
+        switch(count($arg)){
+            case 1:
+  
+                if(is_array($arg[0])){
+
+                    foreach($arg[0] as $key => $value){
+        
+                        $tmp[]="`$key`='$value'";
+
 }
 
-$Journal=new Journal;
+$sql.=" WHERE ". implode(" AND " ,$tmp);
+}else{
+    
+    $sql.=$arg[0];
+}
+break;
+case 2:
+
+foreach($arg[0] as $key => $value){
+
+    $tmp[]="`$key`='$value'";
+
+}
+
+$sql.=" WHERE ". implode(" AND " ,$tmp) . $arg[1];
+break;
+
+}
+
+// echo $sql;
+return $this->pdo->query($sql)->fetchColumn();
+}
+/*     public function sum($col,...$arg){
+$sql="SELECT sum({$col}) FROM $this->table ";
+
+//依參數數量來決定進行的動作因此使用switch...case
+switch(count($arg)){
+case 1:
+
+if(is_array($arg[0])){
+
+    foreach($arg[0] as $key => $value){
+
+        $tmp[]="`$key`='$value'";
+
+    }
+
+    $sql.=" WHERE ". implode(" AND " ,$tmp);
+}else{
+    
+    $sql.=$arg[0];
+}
+break;
+case 2:
+
+foreach($arg[0] as $key => $value){
+
+    $tmp[]="`$key`='$value'";
+
+}
+
+$sql.=" WHERE ". implode(" AND " ,$tmp) . $arg[1];
+break;
+
+}
+
+echo $sql;
+return $this->pdo->query($sql)->fetchColumn(0);
+}
+
+
+public function min($col,...$arg){
+$sql="SELECT min({$col}) FROM $this->table ";
+
+//依參數數量來決定進行的動作因此使用switch...case
+switch(count($arg)){
+case 1:
+
+if(is_array($arg[0])){
+
+    foreach($arg[0] as $key => $value){
+
+        $tmp[]="`$key`='$value'";
+
+    }
+
+    $sql.=" WHERE ". implode(" AND " ,$tmp);
+}else{
+    
+    $sql.=$arg[0];
+}
+break;
+case 2:
+
+foreach($arg[0] as $key => $value){
+
+    $tmp[]="`$key`='$value'";
+
+}
+
+$sql.=" WHERE ". implode(" AND " ,$tmp) . $arg[1];
+break;
+
+}
+
+echo $sql;
+return $this->pdo->query($sql)->fetchColumn(0);
+} *///新增或更新資料
+
+//刪除資料
+public function del($id){
+    $sql="DELETE FROM $this->table WHERE ";
+    if(is_array($id)){
+
+        foreach($id as $key => $value){
+    
+            $tmp[]="`$key`='$value'";
+
+        }
+
+        $sql .= implode(' AND ',$tmp);
+
+    }else{
+
+        $sql .= " id='$id'";
+
+    }
+    
+        //echo $sql;
+
+        return $this->pdo->exec($sql);
+    }
+//萬用的查詢
+
+
+}
+
+$Journal=new DB('journal');
+// $Journal=new Journal;
+
 echo "<pre>";
-print_r($Journal->all());
+print_r($Journal->del(2));
+
+// print_r($Journal->all());
+// print_r($Journal->find(填什麼));
+// print_r($Journal->find(['item'=>'早餐']));
+// 請幫我把這個陣列的第0個位置的count的*印出來  [0]['count(*)']; 
+// print_r($Journal->math('count','*',['item'=>'早餐']));
+
+echo "</pre>"; 
+echo "<pre>";
+print_r($Journal->math('sum','money',['item'=>'早餐']));
+echo "</pre>"; 
+echo "<pre>";
+print_r($Journal->math('min','money',['item'=>'早餐']));
+echo "</pre>"; 
+/* echo "<pre>";
+print_r($Journal->all(['item'=>'早餐']));
 echo "</pre>"; 
 
 
@@ -90,3 +276,9 @@ print_r($db->all());
 echo "</pre>"; */
 
 ?>
+
+
+
+
+
+<!-- 這個function會去拿第一個第0個資料 -->
