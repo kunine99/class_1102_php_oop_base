6d6d1f6c6a978840bfda958cc61447fd0d70e163
rong <rongyy94job@gmail.com>
2021-12-13 08:39:11 +0800

    改了一些東西

--- oop.php
+++ oop.php
@@ -1,11 +1,16 @@
 <?php
+interface Sound{
+    public function sound();
+}
 
-class Animal{
+class Animal implements sound{
+
     // 指定成員時後面只能是xxx 或不指定，不建議改成piblic，這樣以後會不知道誰存取它
-    public $name='';
+    protected $name='';
+
 
     protected $age=0;
-    private $heartbeat=0;
+    protected $heartbeat=0;
 
     public function __construct(){
 // 預設值只能在construct這裡面做
@@ -13,7 +18,9 @@ class Animal{
         $this->name='john';
         $this->heartbeat=rand(20,60);
     }
-
+    public function sound(){
+        return "gi";
+    }
 
 public function getName(){
     return $this->name;
@@ -31,22 +38,91 @@ public function setName($name){
 
 }
 
-$animal=new Animal;
 
+/* $animal=new Animal;
+$dog=new Animal; */
+//echo $animal->name;
+//$animal->name="mack";
 // 外部不能去存取內部
-// echo $animal->name;
-// echo $animal->age;
-echo $animal->getName();
-echo "<br>";
-$animal->setName('mack');
-echo $animal->getName();
-echo $animal->getHeartbeat();
- $dog->setName('herry');
- echo $dog->getName();
- //echo $animal->heartbeat;
+//echo $animal->name;
+//echo $animal->age;
 
+// $animal->setName('mack');
+// echo $animal->getName();
+// echo $animal->getHeartbeat();
+// echo "<hr>";
+// $dog->setName('herry');
+// echo $dog->getName(); */
+//echo $animal->heartbeat;
+
+ class Dog extends Animal{
+    protected $hair_color="black";
+
+
+    public function getColor(){
+        return $this->hair_color;
+    }
+    public function sound(){
+        return "汪";
+    }
+    public function setColor($color){
+        return $this->hair_color=$color;
+    }
+
+    public function getName(){
+        return 'my name is '.$this->name;
+    }
+
+}
+class Cat extends Animal{
+    protected $hair_color="black";
+
+
+    public function getColor(){
+        return $this->hair_color;
+    }
+    public function setColor($color){
+        return $this->hair_color=$color;
+    }
+
+    public function getName(){
+        return 'I am '.$this->name;
+    }
+    public function sound(){
+        return "喵";
+    }
+
+}
+
+echo "<h1>狗</h1>";
+$dog=new Dog;
+echo $dog->getName();
+echo "<br>";
+$dog->setName('mack');
+echo "<br>";
+echo $dog->getName();
+echo "<br>";
+echo $dog->getColor();
+echo "<br>";
+echo $dog->sound();
+echo "<br>";
+
+echo "sdfsdfs";
+echo "<h1>貓</h1>";
+$cat=new Cat;
+echo $cat->getName();
+echo "<br>";
+$cat->setName('judy');
+echo "<br>";
+echo $cat->getName();
+echo "<br>";
+echo $cat->getColor();
+echo "<br>";
+echo $cat->sound();
+echo "<br>";
 
 ?>
+
 
 
 
