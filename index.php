8e0e872b2d9f62ce4b7f2089940cce14a9f0008f
rong <rongyy94job@gmail.com>
2022-02-09 16:39:20 +0800

    測試商業經營科

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-include_once "tr_hm copy.php";
+include_once "test.php";
 
-$XXX->save($_POST);
+// $XXX->save($_POST);
+// $aaa=$Students->find(1);
+$aaa=$Students->all([`major` => '商業經營科']);
+// SELECT * FROM `students` WHERE `major` LIKE '商業經營科'
 
-echo $sql;
+
+// echo $aaa;
+print_r($aaa);
+
+
