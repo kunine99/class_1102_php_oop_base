5cd9c3cf8b350e82c013462c331a0dca81b6295a
enyo <rongyy94job@gmail.com>
2022-02-10 22:33:23 +0800

    第二次練習

--- try2.php
+++ try2.php
@@ -2,7 +2,7 @@
 class DB
 {
     //是charset
-    private $dsn = "mysql:host=localhost;charset=utf8;dbname=db01"; 
+    private $dsn = "mysql:host=localhost;charset=utf8;dbname=db01";
     private $root = 'root';
     private $password = '';
     private $table;
@@ -17,39 +17,77 @@ class DB
 
     public function all(...$arg)
     {
-
+        //是$this->table
+        $sql = "select * from $this->table ";
+        switch (count($arg)) {
+            case 1:
+                if (is_array($arg[0])) {
+                    foreach ($arg[0] as $key => $value) {
+                        //"`$key`='$value'"
+                        $tmp[] = "`$key`='$value'";
+                    }
+                    //WHERE前面要空白
+                    $sql .= " WHERE " . implode(" AND ", $tmp);
+                } else {
+                    $sql .= $arg[0];
+                }
+                break;
+            case 2:
+                foreach ($arg[0] as $key => $value) {
+                    ////"`$key`='$value'"
+                    $tmp[] = "`$key`='$value'";
+                }
+                //不要忘記簡寫的.
+                $sql .= " WHERE " . implode(" AND ", $tmp) . " " . $arg[1];
+                //不要忘記
+                break;
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function math($math, $col, ...$arg)
     {
+        
+        $sql = "SELECT $math(`$col`) FROM $this->table ";
+        if (!empty($arg[0])) {
+            //複製all開始
+            foreach ($arg[0] as $key => $value) {
+                //"`$key`='$value'"
+                $tmp[] = "`$key`='$value'";
+            }
+            //WHERE前面要空白
+            $sql .= " WHERE " . implode(" AND ", $tmp);
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+        ////複製all結束，fetchAll(PDO::FETCH_ASSOC)改成fetchColumn()
 
     }
     public function find($id)
     {
         $sql = "select * from $this->table where ";
-
-            if(isset($id)) {
-                foreach ($id as $key => $value) {
-                    $tmp[] = "`$key`='$value'";
-                }
-                $sql .= implode(" AND ", $tmp); 
-            } else {
-                $sql .= "`id`='$id'";
+        //判斷是不是陣列
+        if (is_array($id)) {
+            foreach ($id as $key => $value) {
+                $tmp[] = "`$key`='$value'";
             }
-            return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+            $sql .= implode(" AND ", $tmp);
+        } else {
+            $sql .= "`id`='$id'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
     public function del($id)
     {
         //find複製來，刪掉select * 改成delete
         $sql = "delete from $this->table where ";
-
-        if(isset($id)) {
+        //判斷是不是陣列
+        if (is_array($id)) {
             foreach ($id as $key => $value) {
                 $tmp[] = "`$key`='$value'";
             }
             //把 AND 改成&&
-            $sql .= implode(" && ", $tmp); 
+            $sql .= implode(" && ", $tmp);
         } else {
             $sql .= "`id`='$id'";
         }
@@ -59,19 +97,32 @@ class DB
 
     public function save($array)
     {
+        if (isset($array['id'])) {
+            foreach ($array as $key => $value) {
+                if ($key != 'id') {
+                    //"`$key`='$value'"
+                    $tmp[] = "`$key`='$value'";
+                    
+                
+         }
+        }
+            $sql = "update $this->table set " . implode(',', $tmp) . " where `id`='{$array['id']}'";
 
+        } else {
+            $sql = "insert into $this->table (`" . implode("`,`", array_keys($array)) . "`)values('" . implode("','", $array) . "')";
+            }
+            return $this->pdo->exec($sql);
     }
 
     public function q($sql)
     {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
 
 function to($url)
 {
-    header("location:" . $url); 
+    header("location:" . $url);
 }
 
 date_default_timezone_set("Asia/Taipei");
